8c799183597715bf36bfab411e2f7cf533169f7c
Niket Yende <niket6@gmail.com>
2023-11-26 13:38:38 +0530

    Update USER_KEYPAIR_PATH

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import { homedir } from 'os';
 import { Keypair } from '@solana/web3.js';
 
-const USER_KEYPAIR_PATH = homedir() + "/.config/solana/id-new.json";
+const USER_KEYPAIR_PATH = homedir() + "/.config/solana/id.json";
 export const userKeypair: Keypair = Keypair.fromSecretKey(
     Buffer.from(JSON.parse(readFileSync(USER_KEYPAIR_PATH, "utf-8")))
 );
